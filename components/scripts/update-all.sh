12adc48b06fcb8a8f7695b91767269c6bdc3ce87
Tao Xin <tao@vanjs.org>
2023-08-11 21:44:46 -0700

    Make update-all.sh responsible for e2e of publishing a new versions of vanjs-ui

--- components/scripts/update-all.sh
+++ components/scripts/update-all.sh
@@ -1,7 +1,12 @@
 set -e
 
+npm run build
+npm publish
+
+VER=$(node_modules/node-jq/bin/jq -r '.version' package.json)
+
 for d in $(ls examples); do(
   cd examples/$d
-  deno run --allow-read --allow-write ../../scripts/upgrade-vanjs-ui-version.ts package.json $1
+  deno run --allow-read --allow-write ../../scripts/upgrade-vanjs-ui-version.ts package.json $VER
   npm update vanjs-ui --S
 )done
