a5c0b80612cf3bf83fdda95ed5e65457c601bb41
Tao Xin <tao@vanjs.org>
2023-08-18 16:36:27 -0700

    1.1.0: A big improvement on GC

--- components/scripts/update-all.sh
+++ components/scripts/update-all.sh
@@ -2,11 +2,12 @@ set -e
 
 npm run build
 npm publish
+npm prune
 
 VER=$(node_modules/node-jq/bin/jq -r '.version' package.json)
 
 for d in $(ls examples); do(
   cd examples/$d
-  deno run --allow-read --allow-write ../../scripts/upgrade-vanjs-ui-version.ts package.json $VER
+  deno run --allow-read --allow-write ../../scripts/upgrade-dep-version.ts package.json vanjs-ui $VER
   npm prune
 )done
--- src/update-vanjs-npm.sh
+++ src/update-vanjs-npm.sh
@@ -1,6 +1,9 @@
 set -e
 
+VER=$(../node_modules/node-jq/bin/jq -r '.version' ../package.json)
+
 cd ../npm-examples/hello
+deno run --allow-read --allow-write ../../components/scripts/upgrade-dep-version.ts package.json vanjs-core $VER
 npm update vanjs-core -S
 
 cd ../../components
