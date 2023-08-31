8e58b7b3ba3e91dd0b15e8923ea6e66a13ec0289
cqh963852 <cqh963852@hotmail.com>
2023-08-31 09:35:15 +0800

    fix: update createState type

--- addons/van_jsx/src/index.d.ts
+++ addons/van_jsx/src/index.d.ts
@@ -1,13 +1,10 @@
 import { State, StateView } from "vanjs-core";
 
-export interface ReadonlyState<T> extends StateView<T> {
-  readonly val: T;
-}
 export declare const lazy: <T>(f: () => T) => T;
 export declare function createState<T>(initialValue: T): State<T>;
 export declare function createState<T>(
   initialValue: T | null
-): ReadonlyState<T>;
+): StateView<T>;
 export declare function createState<T = undefined>(): State<T | undefined>;
 export * from "./jsx-runtime";
 export * from "./type";
