c298f966d1e48cb0fccbffd830fc5852acefb281
yahia-2010 <yahia.berashish@icloud.com>
2023-10-10 06:21:42 +0300

    fix: add ChildDom type to ComponentChild and FunctionChild

--- addons/van_jsx/src/type.d.ts
+++ addons/van_jsx/src/type.d.ts
@@ -1,19 +1,23 @@
-import { State } from "vanjs-core";
+import { State, ChildDom } from "vanjs-core";
+
+type VanElement = Element | ChildDom;
 
 export type PropsWithChildren<P> = P & {
   children?: ComponentChildren | undefined;
 };
 
 export interface FunctionComponent<P = {}> {
-  (props: PropsWithChildren<P>, context?: any): Element | Element[] | null;
+  (props: PropsWithChildren<P>, context?: any):
+    | VanElement
+    | VanElement[]
+    | null;
 }
 
 export type ComponentType<P = {}> = FunctionComponent<P>;
-export type FunctionChild<P> = (props: P) => Element;
+export type FunctionChild<P> = (props: P) => VanElement;
 export type ComponentChild =
-  | State
   | FunctionChild<any>
-  | Element
+  | VanElement
   | string
   | number
   | bigint
