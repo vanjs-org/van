0eced22a464186257ccc57629c5b960216eb09c5
yahia-2010 <yahia.berashish@icloud.com>
2023-10-10 13:41:12 +0300

    enhance: replace ComponentChildren type with ChildDom

--- addons/van_jsx/src/type.d.ts
+++ addons/van_jsx/src/type.d.ts
@@ -1,27 +1,12 @@
 import { State, ChildDom } from "vanjs-core";
 
-type VanElement = Element | ChildDom;
-
-export type PropsWithChildren<P> = P & {
-  children?: ComponentChildren | undefined;
+export type PropsWithChildren<TProps> = TProps & {
+  children?: ChildDom | undefined;
 };
 
-export interface FunctionComponent<P = {}> {
-  (props: PropsWithChildren<P>, context?: any):
-    | VanElement
-    | VanElement[]
-    | null;
+export interface FunctionComponent<TProps = {}> {
+  (props: PropsWithChildren<TProps>, context?: any): ChildDom | null;
 }
 
-export type ComponentType<P = {}> = FunctionComponent<P>;
-export type FunctionChild<P> = (props: P) => VanElement;
-export type ComponentChild =
-  | FunctionChild<any>
-  | VanElement
-  | string
-  | number
-  | bigint
-  | boolean
-  | null
-  | undefined;
-export type ComponentChildren = ComponentChild[] | ComponentChild;
+export type ComponentType<TProps = {}> = FunctionComponent<TProps>;
+export type FunctionChild<TProps> = (props: TProps) => ChildDom;
