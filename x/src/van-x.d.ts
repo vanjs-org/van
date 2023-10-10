039a912b1dfb102d6074d60393e747c9fec2ad59
Tao Xin <tao@vanjs.org>
2023-10-10 08:58:39 -0700

    For StateOf<T>, avoid nested states

--- x/src/van-x.d.ts
+++ x/src/van-x.d.ts
@@ -5,12 +5,10 @@ type TransformField<F> = Reactive<F extends () => infer R ? R : F>
 type KnownNonObjectTypes = string | number | boolean | bigint | symbol | null | undefined
 
 type StatesOf<T> = T extends KnownNonObjectTypes ?
-  T :
-  { [K in keyof T]: State<StatesOf<TransformField<T[K]>>> }
+  T : { [K in keyof T]: State<TransformField<T[K]>> }
 
 type Reactive<T> = T extends KnownNonObjectTypes ?
-  T :
-  { [K in keyof T]: TransformField<T[K]> }
+  T : { [K in keyof T]: TransformField<T[K]> }
 
 export declare const reactive: <T>(obj: T) => Reactive<T>
 export declare const stateFields: <T>(obj: T) => StatesOf<T>
