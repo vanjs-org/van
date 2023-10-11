26c66ebae156301dd630892ac2529b7ee55069c0
Tao Xin <tao@vanjs.org>
2023-10-10 20:58:10 -0700

    Commit the partial progress

--- x/src/van-x.js
+++ x/src/van-x.js
@@ -3,27 +3,47 @@ import van from "vanjs-core"
 // This file consistently uses `let` keyword instead of `const` for reducing the bundle size.
 
 // Global variables - aliasing some builtin symbols to reduce the bundle size.
-let Obj = Object, protoOf = Obj.getPrototypeOf, objProto = protoOf({}), funcProto = protoOf(protoOf), {state, derive} = van
+let Obj = Object, protoOf = Obj.getPrototypeOf, funcProto = protoOf(protoOf), {state, derive, val} = van
+let statesSym = Symbol(), domsSym = Symbol()
 
-let toReactiveObj = (v, protoOfV = protoOf(v)) => protoOfV === objProto ? reactive(v) : v
+let toReactiveObj = v => v instanceof Obj ? reactive(v) : v
 
-let toState = v => {
-  let protoOfV = protoOf(v)
-  return protoOfV === funcProto ?
-    derive(() => toReactiveObj(v())) : state(toReactiveObj(v, protoOfV))
-}
-
-let statesSym = Symbol()
+let toState = v => protoOf(v ?? 0) === funcProto ?
+  derive(() => toReactiveObj(v())) : state(toReactiveObj(v))
 
 let reactive = srcObj => new Proxy(
   Obj.fromEntries(Obj.entries(srcObj).map(([k, v]) => [k, toState(v)])),
   {
-    get: (obj, name) => name === statesSym ? obj : obj[name].val,
-    set: (obj, name, val) => name in obj ?
-      (obj[name].val = toReactiveObj(val), 1) : Reflect.set(obj, name, state(toReactiveObj(val)))
-  }
+    get: (obj, name) => name === statesSym ? obj : val(obj[name]),
+    set: (obj, name, v) => name in obj ?
+      (obj[name].val = toReactiveObj(v), 1) : Reflect.set(obj, name, state(toReactiveObj(v)))
+  },
 )
 
 let stateFields = obj => obj[statesSym]
+
+let withDeleteHandler = srcObj => new Proxy(srcObj,
+  {
+    deleteProperty: (obj, name) =>
+      Reflect.deleteProperty(obj, name) && (obj[domsSym][name].remove(), 1)
+  },
+)
+
+let keyedItems = (containerFunc, s, itemFunc) => {
+  let derived = derive(() => withDeleteHandler(s.val[statesSym] ?? s.val))
+  return dom => {
+    let obj = derived.val
+    if (!dom) {
+      let doms = obj[domsSym] = {}
+      return containerFunc(Obj.entries(obj).map(
+        ([k, v]) => doms[k] = itemFunc(v, () => delete obj[k])))
+    }
+
+    let oldObj = derived.oldVal, oldDoms = oldObj[domsSym]
+    for (let k in oldObj) k in obj || delete oldObj[k]
+
+    return dom
+  }
+}
 
 export {reactive, stateFields}
