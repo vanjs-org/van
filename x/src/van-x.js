a1d6dff595ea6146321cbec76b1fee6572207bfc
Tao Xin <tao@vanjs.org>
2023-10-12 09:57:03 -0700

    Direct alias functions under Reflect

--- x/src/van-x.js
+++ x/src/van-x.js
@@ -3,7 +3,7 @@ import van from "vanjs-core"
 // This file consistently uses `let` keyword instead of `const` for reducing the bundle size.
 
 // Global variables - aliasing some builtin symbols to reduce the bundle size.
-let Obj = Object, Ref = Reflect, Sym = Symbol, {state, derive} = van
+let Obj = Object, {get: refGet, set: refSet, deleteProperty: refDelete} = Reflect, Sym = Symbol, {state, derive} = van
 let statesSym = Sym(), isCalcFunc = Sym(), domsSym = Sym()
 
 let calc = f => (f[isCalcFunc] = 1, f)
@@ -15,15 +15,15 @@ let reactive = srcObj => {
   let proxy = new Proxy(
     (srcObj[statesSym] = new Proxy(
       Obj.fromEntries(Obj.entries(srcObj).map(([k, v]) => [k, toState(v)])),
-      { deleteProperty: (states, name) => Ref.deleteProperty(states, name) && Ref.deleteProperty(srcObj, name) }
+      { deleteProperty: (states, name) => refDelete(states, name) && refDelete(srcObj, name) }
     ), srcObj),
     {
-      get: (obj, name) => obj[statesSym][name]?.val ?? Ref.get(obj, name, proxy),
+      get: (obj, name) => obj[statesSym][name]?.val ?? refGet(obj, name, proxy),
       set: (obj, name, v) => {
         let states = obj[statesSym]
         return name in states ? (states[name].val = reactive(v), 1) : (
-          name in obj || Ref.set(states, name, toState(v)),
-          Ref.set(obj, name, v)
+          name in obj || refSet(states, name, toState(v)),
+          refSet(obj, name, v)
         )
       }
     },
@@ -35,8 +35,7 @@ let stateFields = obj => obj[statesSym]
 
 let withDeleteHandler = srcObj => new Proxy(srcObj,
   {
-    deleteProperty: (obj, name) =>
-      Ref.deleteProperty(obj, name) && (obj[domsSym][name].remove(), 1)
+    deleteProperty: (obj, name) => refDelete(obj, name) && (obj[domsSym][name].remove(), 1)
   },
 )
 
