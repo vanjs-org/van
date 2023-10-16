8e49dea00757dbafa90acc67230fba6465f29648
Tao Xin <tao@vanjs.org>
2023-10-16 10:16:59 -0700

    Add test for array splice

--- x/test/van-x.test.js
+++ x/test/van-x.test.js
@@ -252,6 +252,24 @@ window.runTests = async (van, vanX, file) => {
             assertEq(hiddenDom.innerHTML, '<ul><li>f<button>❌</button></li><li>b<button>❌</button></li><li>d<button>❌</button></li><li>e<button>❌</button></li></ul>');
             assertEq(Object.keys(items).toString(), "0,1,3,4");
         }),
+        list_arraySplice: withHiddenDom(async (hiddenDom) => {
+            const items = vanX.reactive(["a", "b", "c", "d", "e"]);
+            van.add(hiddenDom, vanX.list(ul, items, (v, deleter) => li(v, button({ onclick: deleter }, "❌"))));
+            assertEq(hiddenDom.innerHTML, '<ul><li>a<button>❌</button></li><li>b<button>❌</button></li><li>c<button>❌</button></li><li>d<button>❌</button></li><li>e<button>❌</button></li></ul>');
+            const deleteBtns = hiddenDom.querySelectorAll("button");
+            deleteBtns[2].click();
+            await sleep(waitMsOnDomUpdates);
+            assertEq(hiddenDom.innerHTML, '<ul><li>a<button>❌</button></li><li>b<button>❌</button></li><li>d<button>❌</button></li><li>e<button>❌</button></li></ul>');
+            assertEq(Object.keys(items).toString(), "0,1,3,4");
+            assertEq(items.splice(1, 1).toString(), "b");
+            await sleep(waitMsOnDomUpdates);
+            assertEq(hiddenDom.innerHTML, '<ul><li>a<button>❌</button></li><li>d<button>❌</button></li><li>e<button>❌</button></li></ul>');
+            assertEq(Object.keys(items).toString(), "0,2,3");
+            assertEq(items.splice(1, 2, "f").toString(), ",d");
+            await sleep(waitMsOnDomUpdates);
+            assertEq(hiddenDom.innerHTML, '<ul><li>a<button>❌</button></li><li>f<button>❌</button></li><li>e<button>❌</button></li></ul>');
+            assertEq(Object.keys(items).toString(), "0,1,2");
+        }),
         list_array_multipleBindings: withHiddenDom(async (hiddenDom) => {
             const items = vanX.reactive([1, 2, 3]);
             van.add(hiddenDom, vanX.list(div, items, v => div(v, button({ onclick: () => ++v.val }, "👍"))), vanX.list(ul, items, (v, deleter) => li(v, button({ onclick: deleter }, "❌"))));
--- x/test/van-x.test.ts
+++ x/test/van-x.test.ts
@@ -336,6 +336,30 @@ window.runTests = async (van: Van, vanX: typeof vanXObj, file: string) => {
       assertEq(Object.keys(items).toString(), "0,1,3,4")
     }),
 
+    list_arraySplice: withHiddenDom(async hiddenDom => {
+      const items = vanX.reactive(["a", "b", "c", "d", "e"])
+      van.add(hiddenDom, vanX.list(ul, items,
+        (v, deleter) => li(v, button({onclick: deleter}, "❌"))))
+
+      assertEq(hiddenDom.innerHTML, '<ul><li>a<button>❌</button></li><li>b<button>❌</button></li><li>c<button>❌</button></li><li>d<button>❌</button></li><li>e<button>❌</button></li></ul>')
+
+      const deleteBtns = hiddenDom.querySelectorAll("button")
+      deleteBtns[2].click()
+      await sleep(waitMsOnDomUpdates)
+      assertEq(hiddenDom.innerHTML, '<ul><li>a<button>❌</button></li><li>b<button>❌</button></li><li>d<button>❌</button></li><li>e<button>❌</button></li></ul>')
+      assertEq(Object.keys(items).toString(), "0,1,3,4")
+
+      assertEq(items.splice(1, 1).toString(), "b")
+      await sleep(waitMsOnDomUpdates)
+      assertEq(hiddenDom.innerHTML, '<ul><li>a<button>❌</button></li><li>d<button>❌</button></li><li>e<button>❌</button></li></ul>')
+      assertEq(Object.keys(items).toString(), "0,2,3")
+
+      assertEq(items.splice(1, 2, "f").toString(), ",d")
+      await sleep(waitMsOnDomUpdates)
+      assertEq(hiddenDom.innerHTML, '<ul><li>a<button>❌</button></li><li>f<button>❌</button></li><li>e<button>❌</button></li></ul>')
+      assertEq(Object.keys(items).toString(), "0,1,2")
+    }),
+
     list_array_multipleBindings: withHiddenDom(async hiddenDom => {
       const items = vanX.reactive([1, 2, 3])
       van.add(hiddenDom,
