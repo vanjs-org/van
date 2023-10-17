779281f5c2c33861cda7aa4e5c53a2eaf400a743
Tao Xin <tao@vanjs.org>
2023-10-17 08:41:58 -0700

    Simplify the code a little bit

--- x/examples/list-advanced/src/main.ts
+++ x/examples/list-advanced/src/main.ts
@@ -15,33 +15,29 @@ const TodoList = () => {
   return div(
     div(inputDom, button(
       {onclick: () => items["k" + ++id] = {text: inputDom.value!, done: false}}, "Add")),
-    vanX.list(div, items, ({val: v}, deleter) => {
-      return div(
-        input({type: "checkbox", checked: () => v.done,
-          onclick: e => v.done = e.target.checked}), " ",
-        input({
-          type: "text", value: () => v.text,
-          style: () => v.done ? "text-decoration: line-through;" : "",
-          oninput: e => v.text = e.target.value,
-        }, () => v.text), " ",
-        a({onclick: deleter}, "❌"),
-      )
-    }),
+    vanX.list(div, items, ({val: v}, deleter) => div(
+      input({type: "checkbox", checked: () => v.done,
+        onclick: e => v.done = e.target.checked}), " ",
+      input({
+        type: "text", value: () => v.text,
+        style: () => v.done ? "text-decoration: line-through;" : "",
+        oninput: e => v.text = e.target.value,
+      }, () => v.text), " ",
+      a({onclick: deleter}, "❌"),
+    )),
     div(
       button({onclick: () => vanX.replace(items, l => l.filter(([_, v]) => !v.done))},
         "Clear Completed"),
       button({onclick: () => vanX.replace(items, l =>
-        l.sort(([_1, {text: t1}], [_2, {text: t2}]) => t1.localeCompare(t2)))},
-        "A -> Z"),
+        l.sort(([_1, {text: t1}], [_2, {text: t2}]) => t1.localeCompare(t2)))}, "A -> Z"),
       button({onclick: () => vanX.replace(items, l =>
-        l.sort(([_1, {text: t1}], [_2, {text: t2}]) => t2.localeCompare(t1)))},
-        "Z -> A"),
+        l.sort(([_1, {text: t1}], [_2, {text: t2}]) => t2.localeCompare(t1)))}, "Z -> A"),
       button({onclick: () => vanX.replace(items, l =>
         l.flatMap(([k1, v1]) => [
           [k1, v1],
-          ["k" + ++id, {text: v1.text + " - copy", done: v1.done}]
-        ]))
-      }, "Duplicate List"),
+          ["k" + ++id, {text: v1.text + " - copy", done: v1.done}],
+        ]))},
+        "Duplicate List"),
       button({onclick: () => Object.values(items).forEach(v => v.text += "!")}, 'Append "!"'),
     ),
   )
