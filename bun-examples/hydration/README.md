24b5edf451c11ffe8946bdf2cccbdb1586e5a8ae
Tao Xin <tao@vanjs.org>
2023-10-19 09:14:52 -0700

    Update VanJS version to 1.2.2

--- bun-examples/hydration/README.md
+++ bun-examples/hydration/README.md
@@ -8,7 +8,7 @@ This is a [Bun 1.0](https://bun.sh/blog/bun-v1.0)-based variation of the fullsta
 ```json
   "dependencies": {
     "mini-van-plate": "^0.5.0",
-    "vanjs-core": "^1.2.1"
+    "vanjs-core": "^1.2.2"
   },
   "devDependencies": {
     "bun": "^1.0.0",
--- src/update-vanjs-npm.sh
+++ src/update-vanjs-npm.sh
@@ -9,16 +9,37 @@ deno run --allow-read --allow-write ../../components/scripts/upgrade-dep-version
 npm update vanjs-core -S
 npm prune
 
+cd ../../demo/terminal
+deno run --allow-read --allow-write ../../components/scripts/upgrade-dep-version.ts package.json vanjs-core $VER
+npm update vanjs-core -S
+npm prune
+
 cd ../../components
+deno run --allow-read --allow-write ../components/scripts/upgrade-dep-version.ts package.json vanjs-core $VER
 npm update vanjs-core -S
 npm prune
 
 for d in $(ls examples); do(
   cd examples/$d
+  deno run --allow-read --allow-write ../../../components/scripts/upgrade-dep-version.ts package.json vanjs-core $VER
   npm update vanjs-core -S
   npm prune
 )done
 
 cd ../bun-examples/hydration
+deno run --allow-read --allow-write ../../components/scripts/upgrade-dep-version.ts package.json vanjs-core $VER
 npm update vanjs-core -S
 npm prune
+
+cd ../../x
+
+deno run --allow-read --allow-write ../components/scripts/upgrade-dep-version.ts package.json vanjs-core $VER
+npm update vanjs-core -S
+npm prune
+
+for d in $(ls examples); do(
+  cd examples/$d
+  deno run --allow-read --allow-write ../../../components/scripts/upgrade-dep-version.ts package.json vanjs-core $VER
+  npm update vanjs-core -S
+  npm prune
+)done
