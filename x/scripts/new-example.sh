4eb7be2d53a53d84a23fcb231df99a1f81a2151f
Tao Xin <tao@vanjs.org>
2023-10-20 11:08:31 -0700

    Fix package names

--- x/scripts/new-example.sh
+++ x/scripts/new-example.sh
@@ -9,4 +9,4 @@ cp examples/list-basic/index.html examples/$1
 cp -r examples/list-basic/public examples/$1
 cp examples/list-basic/tsconfig.json examples/$1/tsconfig.json
 
-deno run --allow-read --allow-write scripts/replace-string.ts examples/$1/package.json "van-x-example-list-basic" "vanjs-x-example-$1"
+deno run --allow-read --allow-write scripts/replace-string.ts examples/$1/package.json "van-x-example-list-basic" "van-x-example-$1"
