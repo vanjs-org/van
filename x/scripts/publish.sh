f02eb27919c871e95c9580d26dda42e0d3565ab5
Tao Xin <tao@vanjs.org>
2023-11-02 10:14:47 -0700

    Publish `.d.ts` file to VanJS website as well

--- x/scripts/publish.sh
+++ x/scripts/publish.sh
@@ -2,5 +2,7 @@ set -e
 
 npm publish || true
 npm prune
-node_modules/node-jq/bin/jq -r -j '.version' package.json > ../../vanjs-org.github.io/code/van-x.version
+VER=$(node_modules/node-jq/bin/jq -r -j '.version' package.json)
+echo -n $VER > ../../vanjs-org.github.io/code/van-x.version
 cp dist/van-x.nomodule.min.js ../../vanjs-org.github.io/code
+cp src/van-x.d.ts ../../vanjs-org.github.io/code/van-x-$VER.d.ts
