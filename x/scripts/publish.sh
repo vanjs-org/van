53854eb555fecdee9d22b13858f534398072e8a8
Tao Xin <tao@vanjs.org>
2024-04-12 09:14:35 -0700

    Publish the versioned output for VanX

--- x/scripts/publish.sh
+++ x/scripts/publish.sh
@@ -4,6 +4,6 @@ npm publish || true
 npm prune
 VER=$(node_modules/node-jq/bin/jq -r '.version | split("-")[0]' package.json)
 echo -n $VER > ../../vanjs-org.github.io/code/van-x.version
-cp dist/van-x.nomodule.js ../../vanjs-org.github.io/code
-cp dist/van-x.nomodule.min.js ../../vanjs-org.github.io/code
+cp dist/van-x.nomodule.js ../../vanjs-org.github.io/code/van-x-$VER.nomodule.js
+cp dist/van-x.nomodule.min.js ../../vanjs-org.github.io/code/van-x-$VER.nomodule.min.js
 cp src/van-x.d.ts ../../vanjs-org.github.io/code/van-x-$VER.d.ts
