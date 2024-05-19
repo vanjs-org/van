0b1708b2b6756687baaff13eae6d08e5ec7caa2e
Damien Flament <damien.flament@gmx.com>
2024-05-20 01:53:24 +0200

    Add missing jsxs function

--- addons/van_jsx/src/index.js
+++ addons/van_jsx/src/index.js
@@ -2,4 +2,4 @@ import van from "vanjs-core";
 export function createState(v) {
     return van.state(v);
 }
-export { default as createElement, default as jsx, default as jsxDEV, } from "./createElement";
+export { default as createElement, default as jsx, default as jsxs, default as jsxDEV, } from "./createElement";
