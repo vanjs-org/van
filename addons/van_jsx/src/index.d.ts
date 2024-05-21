3e08b788f9a9593917bf63f2d3ddfc2bb6c25960
Damien Flament <damien.flament@gmx.com>
2024-05-21 23:00:17 +0200

    Add missing jsxs function typing

--- addons/van_jsx/src/index.d.ts
+++ addons/van_jsx/src/index.d.ts
@@ -2,5 +2,5 @@ import { State, StateView } from "vanjs-core";
 export declare function createState<T>(initialValue: T): State<T>;
 export declare function createState<T>(initialValue: T | null): StateView<T>;
 export declare function createState<T = undefined>(): State<T | undefined>;
-export { default as createElement, default as jsx, default as jsxDEV, } from "./createElement";
+export { default as createElement, default as jsx, default as jsxs, default as jsxDEV, } from "./createElement";
 export * from "./type";
