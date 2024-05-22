1d8646880a8bea5d110967e39afa192332c3c78e
Damien Flament <damien.flament@gmx.com>
2024-05-22 07:07:13 +0200

    Add missing Fragment function typing.
    Fixes #324.

--- addons/van_jsx/src/index.d.ts
+++ addons/van_jsx/src/index.d.ts
@@ -2,5 +2,6 @@ import { State, StateView } from "vanjs-core";
 export declare function createState<T>(initialValue: T): State<T>;
 export declare function createState<T>(initialValue: T | null): StateView<T>;
 export declare function createState<T = undefined>(): State<T | undefined>;
+export declare function Fragment({ children }: { children?: VanNode | undefined }): VanNode | [VanNode] | undefined;
 export { default as createElement, default as jsx, default as jsxs, default as jsxDEV, } from "./createElement";
 export * from "./type";
