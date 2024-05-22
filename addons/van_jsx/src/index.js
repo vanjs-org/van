2234b760a6a77d55d828d30554680304fb9b5cfd
Damien Flament <damien.flament@gmx.com>
2024-05-22 07:04:21 +0200

    Add missing Fragment function.

--- addons/van_jsx/src/index.js
+++ addons/van_jsx/src/index.js
@@ -2,4 +2,7 @@ import van from "vanjs-core";
 export function createState(v) {
     return van.state(v);
 }
+export function Fragment({children}) {
+    return children;
+}
 export { default as createElement, default as jsx, default as jsxs, default as jsxDEV, } from "./createElement";
