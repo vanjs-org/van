0c02098d49f9b150291048564ef4788d30155c03
Vlad Sirenko <sirenkovladd@gmail.com>
2024-05-28 00:27:01 -0700

    example for scope mini-van-plate

--- bun-examples/hydration/src/client.ts
+++ bun-examples/hydration/src/client.ts
@@ -1,14 +1,10 @@
-import van from "vanjs-core"
-import Hello from "./components/hello.js"
-import Counter from "./components/counter.js"
-
-const {button, p} = van.tags
-
-van.add(document.getElementById("hello-container")!, Hello({van}))
+import {vanWrap, vanWrapper} from "mini-van-plate/shared";
+import vanOriginal from "vanjs-core";
+import Counter from "./components/counter.js";
+import Hello from "./components/hello.js";
 
 const hydrate = () => {
-  van.hydrate(document.getElementById("basic-counter")!, dom => Counter({
-    van,
+  vanOriginal.hydrate(document.getElementById("basic-counter")!, dom => Counter({
     id: dom.id,
     init: Number(dom.getAttribute("data-counter")),
   }))
@@ -16,12 +12,15 @@ const hydrate = () => {
   const styleSelectDom = <HTMLSelectElement>document.getElementById("button-style")
   const buttonStyle = van.state(styleSelectDom.value)
   styleSelectDom.oninput = e => buttonStyle.val = (<HTMLSelectElement>e.target).value
-  van.hydrate(document.getElementById("styled-counter")!, dom => Counter({
-    van,
+  vanOriginal.hydrate(document.getElementById("styled-counter")!, dom => Counter({
     id: dom.id,
     init: Number(dom.getAttribute("data-counter")),
     buttonStyle,
   }))
 }
 
-van.add(document.getElementById("counter-container")!, p(button({onclick: hydrate}, "Hydrate")))
+vanWrap(vanOriginal, (van) => {
+  const {button, p} = van.tags
+  van.add(document.getElementById("hello-container")!, Hello({}))
+  van.add(document.getElementById("counter-container")!, p(button({onclick: vanWrapper(hydrate)}, "Hydrate")))
+})
